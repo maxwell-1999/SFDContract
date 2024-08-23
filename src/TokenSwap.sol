b1eb39b282ae56141cf0f615953224b723f5f267
maxwell-1999 <maxwell@buffer.finance>
2024-08-23 19:15:29 +0530

    WIP

--- src/TokenSwap.sol
+++ src/TokenSwap.sol
@@ -8,6 +8,7 @@ interface IERC20d {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function burn(uint256 amount) external;
+    function balanceOf(address account) external view returns (uint256);
 }
 
 contract TokenSwap {
@@ -17,29 +18,17 @@ contract TokenSwap {
     constructor(address _tokenA, address _tokenB) {
         tokenA = EsBFR(_tokenA);
         tokenB = IERC20d(_tokenB);
-    }
 
+    }
     function deposit(uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
         tokenA.burn(msg.sender, amount);
+        uint256 tokenBbalance = tokenB.balanceOf(address(this));
+        require(tokenBbalance >= amount/2, "Not enough funds in contract");
         uint256 amountToSend = amount / 2;
         tokenB.transfer(msg.sender, amountToSend);
     }
 
-
-    // function deposit(uint256 amount) external {
-    //     require(amount > 0, "Amount must be greater than 0");
-
-    //     // Transfer Token A from the caller to this contract
-    //     tokenA.transferFrom(msg.sender, address(this), amount);
-
-    //     // Burn Token A
-    //     tokenA.burn(amount);
-
-    //     // Send 0.5 * amount of Token B back to the caller
-    //     uint256 amountToSend = amount / 2;
-    //     tokenB.transfer(msg.sender, amountToSend);
-    // }
 }
 
 
